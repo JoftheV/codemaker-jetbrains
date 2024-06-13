4454b2eb7440c665a6408d37a0618660a3c6d3de
codemakerai-dev <codemaker-dev@codemaker.ai>
2024-06-13 12:35:25 -0700

    Using Kotlin HashMap

--- src/main/kotlin/ai/codemaker/jetbrains/inline/listener/CodemakerEditorFactoryListener.kt
+++ src/main/kotlin/ai/codemaker/jetbrains/inline/listener/CodemakerEditorFactoryListener.kt
@@ -16,21 +16,21 @@ import com.intellij.openapi.editor.event.*
 import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.psi.PsiDocumentManager
 import java.util.*
+import kotlin.collections.HashSet
 
 class CodemakerEditorFactoryListener : EditorFactoryListener {
 
     private val caretListener by lazy { CodemakerCaretListener() }
     private val documentListener by lazy { CodemakerDocumentListener() }
 
-    private val files = Collections.newSetFromMap(IdentityHashMap<String, Boolean>())
+    private val files = HashSet<String>()
 
     override fun editorCreated(event: EditorFactoryEvent) {
-        if (event.editor.virtualFile != null && !files.contains(event.editor.virtualFile.path)) {
+        if (event.editor.virtualFile != null && !files.add(event.editor.virtualFile.path)) {
             event.editor.apply {
                 document.addDocumentListener(documentListener)
                 caretModel.addCaretListener(caretListener)
             }
-            files.add(event.editor.virtualFile.path)
         }
     }
 
